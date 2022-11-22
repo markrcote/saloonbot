fbeccd1ae8294ca83f85598c101a3c34ede5c6c5
Mark Côté <mark.cote@uwaterloo.ca>
2022-11-22 07:30:52 -0500

    Remove testing guild id.

--- bot.py
+++ bot.py
@@ -5,15 +5,13 @@ from nextcord.ext import commands
 
 from wwnames import WildWestNames
 
-TESTING_GUILD_ID = 770741674740678666
-
 bot = commands.Bot()
 
 @bot.event
 async def on_ready():
     print(f'Howdy folks.')
 
-@bot.slash_command(description='Generate a name', guild_ids=[TESTING_GUILD_ID])
+@bot.slash_command(description='Generate a name')
 async def wwname(interaction: nextcord.Interaction, gender: str=''):
     names = WildWestNames()
     await interaction.send(names.random_name(gender))

fa1044a1d5ba8e932150fe307f33b9f0fb3b4838
Mark Côté <mark.cote@uwaterloo.ca>
2023-08-16 22:58:38 -0400

    Add unit tests.

---
+++ test.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch
+from wwnames import WildWestNames
+
+class TestWildWestNames(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.wild_west_names = WildWestNames()
+
+    @patch('random.choice')
+    def test_random_name_male(self, mock_choice):
+        mock_choice.side_effect = ['John', 'Doe']
+        result = self.wild_west_names.random_name(gender='M')
+        self.assertEqual(result, '♂ John Doe')
+
+    @patch('random.choice')
+    def test_random_name_female(self, mock_choice):
+        mock_choice.side_effect = ['Jane', 'Smith']
+        result = self.wild_west_names.random_name(gender='F')
+        self.assertEqual(result, '♀ Jane Smith')
+
+    @patch('random.choice')
+    def test_random_name_random_gender(self, mock_choice):
+        mock_choice.side_effect = ['F', 'Mary', 'Brown']
+        result = self.wild_west_names.random_name()
+        self.assertEqual(result, '♀ Mary Brown')
+
+if __name__ == '__main__':
+    unittest.main()

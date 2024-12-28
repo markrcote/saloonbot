d1286ab50d16a650e401cae4ab516669743ee2a0
Mark Côté <mark.cote@uwaterloo.ca>
2024-12-28 17:14:03 -0500

    Modified to use `asyncio`.
    Not sure about delays after every output.  Overall it works though.

--- cli.py
+++ cli.py
@@ -1,18 +1,23 @@
-import queue
-import signal
-import threading
-from time import sleep
+import asyncio
+
+import aioconsole
 
 from cardgames.card_game import Player
 from cardgames.blackjack import Blackjack
 
-player = None
 game = Blackjack()
-cmd_q = queue.Queue()
-cmd_listener_thread = None
+game.output_func = print
 
 
-def process_command(cmd):
+async def get_command(prompt):
+    line = await aioconsole.ainput('> ' if prompt else '')
+    if line:
+        args = line.strip().split()
+        return (args[0], args[1:])
+    return (None, [])
+
+
+async def process_command(player, cmd, args):
     output = []
 
     if cmd == 'quit':
@@ -31,84 +36,58 @@ def process_command(cmd):
             '  surrender',
         ])
     elif cmd == 'hit':
-        game.hit(player)
-    elif cmd =='stand':
-        game.stand(player)
+        await game.hit(player)
+    elif cmd == 'stand':
+        await game.stand(player)
     elif cmd.startswith('bet'):
         try:
-            amount = int(cmd.split(' ')[1])
+            amount = int(args[0])
         except ValueError:
-                output.append('Invalid bet amount')
+            output.append('Invalid bet amount')
         else:
             game.bet(amount)
-    elif cmd =='split':
+    elif cmd == 'split':
         game.split(player)
     elif cmd == 'double':
         game.double(player)
     elif cmd == 'insurance':
         game.insurance(player)
-    elif cmd =='surrender':
+    elif cmd == 'surrender':
         game.surrender(player)
     else:
         output.append('Unknown command')
     return output
 
 
-def cmd_listener():
-    while True:
-        try:
-            cmd = input('> ')
-        except EOFError:
-            cmd = 'quit'
-        print(f'command: {cmd}')
-        cmd_q.put(cmd)
-        if cmd == 'quit':
-            break
-
-
-def setup_cmd_listener():
-    global cmd_listener_thread
-    cmd_listener_thread = threading.Thread(target=cmd_listener, daemon=True)
-    cmd_listener_thread.start()
-
-
-def teardown_cmd_listener():
-    cmd_listener_thread.join(1)
-
-
-if __name__ == "__main__":
-    output = game.tick()
-    if output:
-        print('\n'.join(output))
+async def main():
+    await game.tick()
 
     player_name = input('Enter your name: ')
     player = Player(player_name)
     print(f'Welcome {player.name}')
-    game.sit_down(player)
+    await game.sit_down(player)
 
-    setup_cmd_listener()
+    quit = False
+    timed_out = False
 
-    try:
-        while True:
-            output = game.tick()
-            if output:
-                print('\n'.join(output))
-            try:
-                cmd = cmd_q.get(timeout=5)
-            except queue.Empty:
-                cmd = None
-            output = None
-            if cmd:
-                output = process_command(cmd)
+    while not quit:
+        cmd = None
+        args = []
 
-            if output:
-                print('\n'.join(output))
-    except KeyboardInterrupt:
-        # Ctrl-C doesn't exactly work here because the cmd_listener thread is
-        # stuck on the blocking input() call.  Fixing this requires a more
-        # complex input routine.
-        pass
+        try:
+            cmd, args = await asyncio.wait_for(get_command(prompt=not timed_out), timeout=1)
+            timed_out = False
+        except asyncio.TimeoutError:
+            timed_out = True
 
-    print('tearing down')
-    teardown_cmd_listener()
-    print('done')
+        await game.tick()
+
+        if cmd:
+            if cmd == 'quit':
+                quit = True
+            else:
+                await process_command(player, cmd, args)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
